e8b4fcd643265d213fae6ceaf9aaf68a14d81a5a
Heather Nuffer <zzzuzz@gmail.com>
2020-04-09 15:14:31 -0600

    Puts binary_search in its own file

---
+++ src/tk/binary_search.py
@@ -0,0 +1,46 @@
+
+"""
+Try writing a Python function to perform a binary search on a set of data.
+"""
+nums = [2, 6, 8.9, 99, 125, 888, 78898, 208765]
+target2 = 78898
+target3 = 6
+
+'''This version assumes that the target IS in the arr'''
+
+
+def binary_search(arr, target):
+    middle = len(nums) // 2
+    is_found = False
+    while not is_found:
+        if nums[middle] == target:
+            is_found = True
+            print(middle)
+        elif nums[middle] < target:
+            middle = middle + middle//2
+        else:
+            middle = middle//2
+
+
+binary_search(nums, target2)
+binary_search(nums, target3)
+
+
+def tk_binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        middle = (low + high)//2
+        if target == arr[middle]:
+            return middle
+        elif target < arr[middle]:
+            high = middle - 1
+        elif target > arr[middle]:
+            low = middle + 1
+    return -1
+
+
+print("Time for tk binary search: ")
+print(tk_binary_search(nums, 99))
+print(tk_binary_search(nums, 888))
+print(tk_binary_search(nums, 800))
