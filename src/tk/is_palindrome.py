247c6122e57219392f507b90a580b70df12ae353
Heather Nuffer <zzzuzz@gmail.com>
2020-04-10 11:33:54 -0600

    Cleans up parameter string in is_palindrome()

--- src/tk/is_palindrome.py
+++ src/tk/is_palindrome.py
@@ -1,3 +1,4 @@
+import string
 from reverse_string import reverse_string
 
 """
@@ -21,9 +22,22 @@ Wassamassaw – a town in South Dakota
 """
 
 
-def is_palindrome(str):
-    return str == reverse_string(str)
+def is_palindrome(mystr):
+    # Take out spaces and punctuation
+    mystr = mystr.replace(' ', '')
+    mystr = mystr.translate(str.maketrans('', '', string.punctuation))
+    # Make everything lowercase
+    mystr = mystr.lower()
+    print(mystr)
+    return mystr == reverse_string(mystr)
 
 
 print(is_palindrome('radar'))
 print(is_palindrome('rootbeer'))
+print(is_palindrome('Reviled did I live, said I, as evil I did deliver'))
+print(is_palindrome("Go hang a salami; I'm a lasagna hog."))
+print(is_palindrome("Live not on evil"))
+print(is_palindrome('Able was I ere I saw Elba'))
+print(is_palindrome('Wassamassaw'))
+print(is_palindrome("Go hang a salami 2; I'm a lasagna hog. 2"))
+print(is_palindrome("2Go hang a salami; I'm a lasagna hog.2"))
