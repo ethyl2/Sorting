247da006b717b4965043fcf315881d3a9a0df553
Heather Nuffer <zzzuzz@gmail.com>
2020-04-13 14:44:25 -0600

    Adds insertion_sort in iterative_sorting.py

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -45,3 +45,41 @@ def bubble_sort2(arr):
 def count_sort(arr, maximum=-1):
 
     return arr
+
+
+"""
+Insertion Sort:
+
+Separate the first element from the rest of the array. Think about it as a sorted list of one element.
+
+For all other indices, beginning with [1]:
+
+a. Copy the item at that index into a temp variable
+
+b. Iterate to the left until you find the correct index in the "sorted" part of the array at which this element should be inserted
+
+Shift items over to the right as you iterate
+c. When the correct index is found, copy temp into this position
+
+"""
+
+
+def insertion_sort(arr):
+    # Divide your hand into sorted on the left and unsorted on the right
+    # Sorted is just the first element
+    # then go card by card and move them into place.
+    # Loop through all elements in unsorted...
+    for i in range(1, len(arr)):
+        temp = arr[i]
+        j = i  # j is our sliding index
+        # Shift sorted to the right until correct position found
+        while j > 0 and temp < arr[j - 1]:
+            arr[j] = arr[j - 1]  # Slide over one element
+            j -= 1
+        # Insert at that position
+        arr[j] = temp
+    return arr
+
+
+my_arr_14 = [6, 14, 13, 7, 14]
+print(insertion_sort(my_arr_14))
